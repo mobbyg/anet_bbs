a8265b69150e6c0ccfcaf6ddb05c13ab536a787f
Rich Lawrence <mobbyg@gmail.com>
2023-08-03 23:45:42 -0400

    Update bbs_gui.py
    Updated gui.

--- bbs_gui.py
+++ bbs_gui.py
@@ -1,44 +1,86 @@
-#!/usr/bin/env python3
+import tkinter as tk
+from tkinter import messagebox
+import tkinter.font as tkfont
 
-"""
-ANet Control Panel : This code creates the GUI for the BBS and is also the telnet server.
+def open_new_window(title):
+    new_window = tk.Toplevel(root)
+    new_window.title(title)
+    new_window.geometry("300x600")
 
-I will slowly move over the code from the BBSCORE module I have already done some coding on.
-"""
+def create_top_row_command(label):
+    def top_row_command():
+        open_new_window(label)
+    return top_row_command
 
-from tkinter import *
-from tkinter import ttk
+def create_node_command(index):
+    def node_command():
+        open_new_window(f"Node {index}")
+    return node_command
 
-root = Tk() #Create Master Window
-root.title("ANet Control Panel") #Give Master Window a title
+def quit_app():
+    confirm = messagebox.askyesno("Confirm Quit", "Are you sure you want to quit?")
+    if confirm:
+        root.destroy()
 
-#Make some buttons and place them. Commands come after..
-sysinfoButton = ttk.Button(root, text = "Sys Info")
-sysinfoButton.grid(row = 0, column = 0)
-sysinfoButton.pack()
+root = tk.Tk()
+root.title("ANet Control Panel")
 
-userinfoButton = ttk.Button(root, text = "User Info")
-userinfoButton.grid(row = 0, column = 1)
+# Set the width and height of the root window
+root.geometry("1000x600")
 
-configButton = ttk.Button(root, text = "Config")
-configButton.grid(row = 0, column = 2)
+# Set the background color of the root window to light gray
+root.configure(bg="light gray")
 
-mailButton = ttk.Button(root, text = "Mail")
-mailButton.grid(row = 0, column = 3)
+# Load the CuppaJoe font for the "node" buttons
+node_font = tkfont.Font(family="CuppaJoe", size=16)
 
-filesButton = ttk.Button(root, text = "Files")
-filesButton.grid(row = 0, column = 4)
+# Create top row buttons
+buttons = [
+    ("SysInfo", 100),
+    ("User Info", 100),
+    ("Config", 100),
+    ("Mail", 100),
+    ("Files", 100),
+    ("Yanks", 100),
+    ("News", 100),
+    ("Edit", 100),
+    ("Quit", 100),
+]
 
-yanksButton = ttk.Button(root, text = "Yanks")
-yanksButton.grid(row = 0, column = 5)
+top_row_commands = [
+    create_top_row_command(label) for label, _ in buttons
+]
 
-newsButton = ttk.Button(root, text = "News")
-newsButton.grid(row = 0, column = 6)
+# Create and place top row buttons on the root window
+for i, (label, width) in enumerate(buttons):
+    btn = tk.Button(root, text=label, command=top_row_commands[i], width=width)
+    btn.grid(row=0, column=i, padx=5, pady=5)
 
-editButton = ttk.Button(root, text = "Edit")
-editButton.grid(row = 0, column = 7)
+# Create a scrollable frame for the node buttons
+canvas = tk.Canvas(root)
+frame = tk.Frame(canvas)
+vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set)
 
-quitButton = ttk.Button(root, text = "Quit")
-quitButton.grid(row = 0, column = 8)
+# Place the scrollable frame on the canvas
+canvas.grid(row=1, column=0, columnspan=len(buttons), sticky="nsew")
+vsb.grid(row=1, column=len(buttons), sticky="ns")
+canvas.create_window((4, 4), window=frame, anchor="nw")
+
+# Create "node" buttons and place them on the scrollable frame
+for i in range(11):  # Create 11 "node" buttons
+    btn = tk.Button(frame, text=str(i), command=create_node_command(i), width=1000, height=3, anchor='w', justify='left', font=node_font)
+    btn.grid(row=i, column=0, padx=5, pady=5, sticky="w")
+
+# Update the canvas scrolling region
+frame.update_idletasks()
+canvas.config(scrollregion=canvas.bbox("all"))
+
+# Configure columns and rows to expand when the window is resized
+for i in range(len(buttons)):
+    root.columnconfigure(i, weight=1)
+
+root.rowconfigure(1, weight=1)
 
 root.mainloop()
+
